57a1f9bfb488afa455da31b3dc86b6bcaecbc764
bsy0720 <df3978@naver.com>
2022-07-05 14:42:07 +0900

    seomun

---
+++ public/js/animation.js
@@ -0,0 +1,40 @@
+// 메인사진 스와이퍼 슬라이드
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+
+// 카카오 로그인
+Kakao.init('4169f24b838b75e164208f2d4095211d');
+Kakao.isInitialized();
+
+function kakaoLogin() {
+    Kakao.Auth.login({
+        success: function (response) {
+            Kakao.API.request({
+                url: '/v2/user/me',
+                success: function (response) {
+                    document.getElementById('user').innerHTML =
+                        response.kakao_account.profile.nickname;
+                }
+            })
+        }
+    })
+}
+
+function kakaoLogout() {
+
+}
